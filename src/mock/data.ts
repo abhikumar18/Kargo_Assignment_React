682bffa9f582ff71ac5d6a7478380b3a649a1722
abhikumar18 <abhishek.it.msit@gmail.com>
2023-09-15 10:36:59 +0100

    first commit

---
+++ src/mock/data.ts
@@ -0,0 +1,39 @@
+export const mockData = [
+    {
+      title: "HTML and CSS",
+      type: 'html5',
+      subtitle:
+        "This beginner-friendly course will teach you the fundamentals of HTML and CSS, two essential languages for structuring and styling websites.",
+    },
+    {
+      title: "Javascript",
+      type: 'js',
+      subtitle:
+        "This comprehensive course will teach you the fundamentals of JavaScript, the programming language that powers the web. You will be able to give life to your websites and create functional websites and web apps.",
+    },
+    {
+      title: "React",
+      type: 'react',
+      subtitle:
+        "This course will teach you the fundamentals of React, a modern JavaScript library for building user interfaces. You will be able to use React to build Single Page Applications and dynamic, interactive user interfaces.",
+    },
+    {
+      title: "Angular",
+      type: 'angular',
+      subtitle:
+        "This course will teach you everything you need to know to build single-page applications with Angular. You will be able to build real-world applications with Angular, using RxJS and Webpack.",
+    },
+    {
+      title: "Vue",
+      type: 'vue',
+      subtitle:
+        "This course will teach you the fundamentals of Vue.js, a progressive JavaScript framework for building user interfaces. You will be able to build real-world applications with Vue.js, using Vuex and Vue Router.",
+    },
+    {
+      title: "Svelte",
+      type: 'svelte',
+      subtitle:
+        "This course will teach you the fundamentals of Svelte, a modern JavaScript framework for building fast and performant web applications. You will be able to build real-world applications with Svelte, using Axios and SvelteKit.",
+    }
+  ];
+  
